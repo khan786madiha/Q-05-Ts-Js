4f73b046aa317dd188dc587d1cd4b6756335d3f1
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:37:40 -0800

    Q-05 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-05

---
+++ Q5.js
@@ -0,0 +1,6 @@
+"use strict";
+// 5. Famouse qoute 2:Repeat Exercise 4, but this is store the famouse persn's name in a veriab
+//  called famouse_persons.Then compose your message and stor it in anew variable called message. Print your message. 
+let Famouse_person = "The Prophet Muhammad صلی اللہ علیہ وسلم ";
+let message = "“Whatever is prayed for at the time of breaking the fast is granted and never refused.”";
+console.log(`${Famouse_person} said, ${message}`);
---
+++ Q5.ts
@@ -0,0 +1,8 @@
+// 5.	Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let Famouse_person: string = "The Prophet Muhammad صلی اللہ علیہ وسلم "
+
+let message: string = "“Whatever is prayed for at the time of breaking the fast is granted and never refused.”"
+
+console.log(`${Famouse_person} said, ${message}`);
